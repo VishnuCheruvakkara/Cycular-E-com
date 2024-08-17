c57e00dc79a0eef3bd836cc84c90a9c5b2fb623f
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2024-08-17 09:22:59 +0530

    added : soft delete of a product was successfully added.

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from products.models import ProductVariant
 #######################  user home-side #####################################3
 
 def Index(request):
-    product_variants=ProductVariant.objects.all()
+    product_variants=ProductVariant.objects.filter(product__status=True)
     context={
         'product_variants':product_variants,
     }
