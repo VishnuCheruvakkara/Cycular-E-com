910c7d7fdb42a22847c741e42d8ef800476467ae
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2024-08-18 22:24:45 +0530

    validation for add product was completed

--- products/forms.py
+++ products/forms.py
@@ -20,6 +20,38 @@ class ProductForm(forms.ModelForm):
             'brand': forms.Select(attrs={'class': 'form-control'}),
             'key_specification': forms.Textarea(attrs={'class': 'form-control'}),
         }
+    def clean(self):
+        cleaned_data = super().clean()
+        name=cleaned_data.get("name")
+        price = cleaned_data.get("price")
+        category = cleaned_data.get("category")
+        key_specification = cleaned_data.get("key_specification")
+         # Validation for the name field
+        if name:
+            if len(name) < 3:
+                self.add_error('name', "Name must be at least 3 characters long.")
+            if not name.isalnum():
+                self.add_error('name', "Name should only contain alphabet or number characters only.")
+            # Check for unique name in the Product model
+            if Product.objects.filter(name=name).exists():
+                self.add_error('name', "A product with this name already exists.")
+        if price:
+            if price <= 0:
+                self.add_error('price', "Price must be a positive number.")
+            if price >=100000:  # Example maximum price
+                self.add_error('price', "Price cannot exceed 1,00,000.")
+           
+
+        if not category:
+            self.add_error('category', "Category is required.")
+
+        if not key_specification:
+            self.add_error('key_specification', "Key specification is required.")
+        if len(key_specification) < 10:
+            self.add_error('key_specification', "Key specification must be at least 10 characters long.")
+        # Additional custom validations can go here
+
+        return cleaned_data
 
 
 class ProductVariantForm(forms.ModelForm):
