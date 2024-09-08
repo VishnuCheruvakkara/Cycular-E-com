1cf6b913715058f073cf7bc3faec7346b8484a9b
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2024-09-08 11:25:32 +0530

    product showed in the checkout page

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from cart.models import Cart,CartItem
 
 # Create your views here.
 
 def check_out(request):
-
-    return render(request,'payment/check-out.html')
+    #retrive the current user's cart
+    cart=get_object_or_404(Cart,user=request.user)
+    #retrive all items for the current user cart
+    cart_items=cart.items.all() #items is the related name
+    
+    #total price of product
+    total_price=sum(item.subtotal for item in cart_items)
+  
+    context={
+        'cart_items':cart_items,
+        'total_price':total_price,
+    }
+    return render(request,'payment/check-out.html',context)
