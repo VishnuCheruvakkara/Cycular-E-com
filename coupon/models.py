605c7e73491c64c08eaa28c610166dab093b4a50
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2024-09-19 12:15:25 +0530

    going for coupon soft delete.

--- coupon/models.py
+++ coupon/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator,MinValueValidator
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
-    discount_value = models.DecimalField(max_digits=5, decimal_places=2,validators=[MinValueValidator(0), MaxValueValidator(100)])  # Percentage value, should be between 0 and 100
+    discount_value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])  # Percentage value, should be between 0 and 100
     valid_from = models.DateTimeField(auto_now_add=True)
     valid_until = models.DateTimeField()
     description=models.CharField(max_length=250,null=True)
@@ -14,3 +14,10 @@ class Coupon(models.Model):
     def __str__(self):
         # Displaying the coupon code and discount percentage
         return f"Coupon {self.code} - {self.discount_value}% off"
+
+    def save(self, *args, **kwargs):
+        if self.valid_until:
+            # If time is not specified, set the time to 12 PM (noon)
+            if self.valid_until.hour == 0 and self.valid_until.minute == 0 and self.valid_until.second == 0:
+                self.valid_until = self.valid_until.replace(hour=12, minute=0, second=0)
+        super().save(*args, **kwargs)
