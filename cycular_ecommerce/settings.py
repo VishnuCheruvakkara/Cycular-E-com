11ab07b93a01807a3f8ffebe5a6421b354fe6f6d
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2026-01-17 15:04:49 +0530

    fix : added whitenoise

--- cycular_ecommerce/settings.py
+++ cycular_ecommerce/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,6 +162,10 @@ STATICFILES_DIRS=[
 MEDIA_URL='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+
+# WhiteNoise storage (compress & cache)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
